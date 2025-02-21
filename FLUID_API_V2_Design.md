5e82f5abfba8318fa831bd57a284cac59285e00d
Brandon Zhu <brandonzhu123@gmail.com>
2025-02-21 13:53:27 -0500

    Get dataset endpoint for cfapi

--- FLUID_API_V2_Design.md
+++ FLUID_API_V2_Design.md
@@ -2,59 +2,58 @@
 
 Issues with version 1:
 
-- Endpoints not organized into separately defined modules
-  - Introduce static urls for parameters that have a fixed set of options (ex: collection, dataset)
-- A single view is handling multiple endpoints, multiple functionality
-- Path parameters not required for certain cases but still require input
-- Too many checks to handle individual cases for path parameters
+-   Endpoints not organized into separately defined modules
+    -   Introduce static urls for parameters that have a fixed set of options (ex: collection, dataset)
+-   A single view is handling multiple endpoints, multiple functionality
+-   Path parameters not required for certain cases but still require input
+-   Too many checks to handle individual cases for path parameters
 
 Potential fixes in version 2:
 
-- Modules and submodules
-  - Separate concerns and responsibilities into different modules
-  - Example modules: fcast, assim
-  - Also helps structure and organize the API Docs generated from FastAPI
-- APIRouter
-  - <https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-apirouter>
-  - Resolves path operations inside a module
-  - Define a prefix that all path urls in this module share:
-    - Prefix= “/fcast”
-    - All views in this module can just use the relative path
-  - SSOT
-- Designate views that solely return a description about current endpoint in JSON
-- Designate views that solely return JSON data from data source (wxmaps)
-- Design philosophy: Focus on creating small, isolated functions with clear single responsibilities
-
+-   Modules and submodules
+    -   Separate concerns and responsibilities into different modules
+    -   Example modules: fcast, assim
+    -   Also helps structure and organize the API Docs generated from FastAPI
+-   APIRouter
+    -   <https://fastapi.tiangolo.com/tutorial/bigger-applications/#another-module-with-apirouter>
+    -   Resolves path operations inside a module
+    -   Define a prefix that all path urls in this module share:
+        -   Prefix= “/fcast”
+        -   All views in this module can just use the relative path
+    -   SSOT
+-   Designate views that solely return a description about current endpoint in JSON
+-   Designate views that solely return JSON data from data source (wxmaps)
+-   Design philosophy: Focus on creating small, isolated functions with clear single responsibilities
 
 Layout of Datasets:
-  - Fcst
-    - Chm
-      - V1
-        - No2
-        - O3
-        - Pm2.5
-        - Hcho
-        - Co
-        - so2
-      - p23
-        - ""
-    - Met
-      - X1
-        - MET
-      - P23
-        - MET
-  - Assim
-    - Aqc
-      - V1
-        - No2
-        - O3
-        - pm2.5
-    - Chm
-      - V1
-      - P23
-      - V72
-    - Met
-      - X1
-      - p23
-      - v72
-  
+
+-   Fcst
+    -   Chm
+        -   V1
+            -   No2
+            -   O3
+            -   Pm2.5
+            -   Hcho
+            -   Co
+            -   so2
+        -   p23
+            -   ""
+    -   Met
+        -   X1
+            -   MET
+        -   P23
+            -   MET
+-   Assim
+    -   Aqc
+        -   V1
+            -   No2
+            -   O3
+            -   pm2.5
+    -   Chm
+        -   V1
+        -   P23
+        -   V72
+    -   Met
+        -   X1
+        -   p23
+        -   v72
--- main.py
+++ main.py
@@ -1,31 +1,113 @@
-from fastapi import FastAPI, Query
-from typing import List, Union
+from fastapi import FastAPI, Query, HTTPException, Depends, Path
+from typing import List, Union, Annotated, Optional
 import xarray as xr
+from enum import Enum
+import glob
+import os
+
 
 app = FastAPI()
+DATASET_DIR = "./dataset_files"
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+class Collection(str, Enum):
+    fcst = "fcst"
+    rpl = "rpl"
 
-@app.get("/data/{lat}/{lon}")
-async def get_data(
-    lat: float,
-    lon: float,
-    parameters: Union[List[str]] = Query(...)
+class Dataset(str, Enum):
+    chm = "chm"
+    ems = "ems"
+    htf = "htf"
+    met = "met"
+    xgc = "xgc"
+    aqc = "aqc"
+
+class Level(str, Enum):
+    v1 = "v1"
+    v72 = "v72"
+    p23 = "p23"
+    x1 = "x1"
+
+class Product(str, Enum):
+    NO2 = "NO2"
+    CO = "CO"
+    O3 = "O3"
+    PM25 = "PM2.5"
+    Hcho = "Hcho"
+    SO2 = "SO2"
+
+
+# DATASET_ALLOWED_OPTIONS = {
+#     Collection.fcst: [{Dataset.chm:
+#                        [{Level.v1: [Product.NO2, Product.O3, Product.PM25, Product.Hcho, Product.CO, Product.SO2]},
+#                        ]},
+#                       {Dataset.met: Level.x1, Dataset.met: Level.p23}]
+    
+# }
+
+allowed_levels = {
+    "met": {"x1", "p23"},
+}
+
+# Dependency to validate level
+def validate_level(dataset: Optional[Dataset] = None, level: Optional[Level] = None):
+    if dataset is None or level is None:
+        return Level.v1
+    
+    if level not in allowed_levels.get(dataset, set()):
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid level '{level}' for dataset '{dataset}'. Allowed: {allowed_levels.get(dataset, set())}"
+        )
+    return level
+
+
+@app.get("/cfapi/{collection}/")
+@app.get("/cfapi/{collection}/{dataset}/")
+@app.get("/cfapi/{collection}/{dataset}/{level}/")
+@app.get("/cfapi/{collection}/{dataset}/{level}/{lat}/{lon}")
+async def get_dataset(
+    collection: Collection = Collection.fcst,
+    dataset: Optional[Dataset] = Dataset.chm,
+    level: Optional[Level] = Depends(validate_level),
+    lat: float = 38,
+    lon: float = -77,
+    products: Annotated[list, "Provide a list of products to subset a particular dataset.", Query(...)] = []
 ):
     # Resolve case sensitivity
-    parameters = [param.upper() for param in parameters]
+    products = [param.upper() for param in products]
 
-    # Open the Netcdf file
-    ds = xr.open_dataset("GEOS-CF.v01.rpl.aqc_tavg_1hr_g1440x721_v1.20250203_0030z.nc4", engine="netcdf4")
+    # TODO: Perform parameter checks with database to verify validity
+
+    # Get filenames that match the parameters
+    filenames = get_file_paths(collection, dataset, level)
+
+    # Handle exception when filename not found
+    if not filenames:
+        raise HTTPException(status_code=404, detail="Dataset not found.")
+
+    file0 = filenames[0]
+
+    # Open the NetCDF file
+    ds = xr.open_dataset(file0, engine="netcdf4")
 
     # Subset the dataset
-    subset = ds.sel(lat=lat, lon=lon, method='nearest')[parameters]
-
-    # Convert to dictionary and return as JSON
-    result = subset.to_dict()
+    try:
+        subset = ds.sel(lat=lat, lon=lon, method='nearest')[products]
+        result = subset.to_dict()
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Invalid product(s).")
 
     return result
+
+
+def get_file_paths(collection, dataset, level):
+    dataset_name_wildcard = f'GEOS-CF.v01.{collection}.{dataset}*{level}*'
+    file_list = glob.glob(os.path.join(DATASET_DIR, dataset_name_wildcard))
+    file_list.sort()
+
+    return file_list
